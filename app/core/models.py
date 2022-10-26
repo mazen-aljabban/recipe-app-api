fc6397ac303f91c87d730f9f0f30a80be7d1d171
TIGER <mezo.tiger.1998a@gmail.com>
2022-10-26 21:02:23 +0300

    added custom user model

---
+++ app/core/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """creates new user"""
+        if not email:
+            raise ValueError('Most have email address.')
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password):
+        """create and save superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user
+    
+    
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """using email instade of uesrname"""
+    email= models.EmailField(max_length=225, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    
